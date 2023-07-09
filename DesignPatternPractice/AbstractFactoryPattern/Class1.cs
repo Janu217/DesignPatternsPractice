d79817913d88c09f728cdba53b8dc6c6bd498cd1
Janaki <Janu217@gmail.com>
2023-07-09 22:22:33 +0530

    Design patterns added

---
+++ DesignPatternPractice/AbstractFactoryPattern/Class1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactoryPattern
+{
+	public class Example
+    {
+		static Example()
+        {
+			Console.WriteLine("Static constructor");
+        }
+
+		public Example()
+        {
+			Console.WriteLine("Non-static constructor");
+        }
+
+		
+    }
+
+	public interface ISourceCourseFactory1
+	{
+		Isource1 GetIsource1();
+		Icourse1 GetIcourse1();
+	}
+
+	public interface Isource1
+	{
+		public void GetDetails();
+	}
+
+	public interface Icourse1
+	{
+		public void GetDetails();
+	}
+
+	public class OnlineSource1 : Isource1
+	{
+		public void GetDetails()
+		{
+			Console.WriteLine("OnlineSource");
+		}
+	}
+
+	public class OfflineSource1 : Isource1
+	{
+		public void GetDetails()
+		{
+			Console.WriteLine("OfflineSource");
+		}
+	}
+
+	public class FrontEndSource1 : Icourse1
+	{
+		public void GetDetails()
+		{
+			Console.WriteLine("FrontEndSource");
+		}
+	}
+
+	public class BackEndEndSource1 : Icourse1
+	{
+		public void GetDetails()
+		{
+			Console.WriteLine("BackEndEndSource");
+		}
+	}
+
+    public class OnLineSourceFrontEndCourseFactory1 : ISourceCourseFactory1
+    {
+        public Icourse1 GetIcourse1()
+        {
+			return new FrontEndSource1();
+        }
+
+        public Isource1 GetIsource1()
+        {
+			return new OnlineSource1();
+
+		}
+    }
+
+	public class OfflineSourceBackEndCourseFactory1 : ISourceCourseFactory1
+	{
+		public Icourse1 GetIcourse1()
+		{
+			return new BackEndEndSource1();
+		}
+
+		public Isource1 GetIsource1()
+		{
+			return new OfflineSource1();
+
+		}
+	}
+
+	
+}
--- DesignPatternPractice/AbstractFactoryPattern/Program.cs
+++ DesignPatternPractice/AbstractFactoryPattern/Program.cs
@@ -6,22 +6,30 @@ namespace AbstractFactoryPattern
     {
         static void Main(string[] args)
         {
-            //The Abstract Factory Design Pattern provides a way to encapsulate a group of individual factories that have a common theme without specifying their concrete classes
-            //Online Source FrondEnd Course
-            OnlineSourceFrondEndCourseFactory onlineSourceFrondEndCourseFactory = new OnlineSourceFrondEndCourseFactory();
-            Console.WriteLine("Source" + onlineSourceFrondEndCourseFactory.GetIsource().getSourceName());
-            Console.WriteLine("Course Duration" + onlineSourceFrondEndCourseFactory.GetCourse().GetCourseDuration());
-            Console.WriteLine("Course Name" + onlineSourceFrondEndCourseFactory.GetCourse().getCourseName());
-            Console.WriteLine("Course Fee" + onlineSourceFrondEndCourseFactory.GetCourse().getCourseFee());
-
-            //Offline Source BackEnd Course
-            OfflineSourceBackEndCourse offlineSourceBackEndCourse = new OfflineSourceBackEndCourse();
-            Console.WriteLine("Source" + offlineSourceBackEndCourse.GetIsource().getSourceName());
-            Console.WriteLine("Course Duration" + offlineSourceBackEndCourse.GetCourse().GetCourseDuration());
-            Console.WriteLine("Course Name" + offlineSourceBackEndCourse.GetCourse().getCourseName());
-            Console.WriteLine("Course Fee" + offlineSourceBackEndCourse.GetCourse().getCourseFee());
-
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Main Method Execution Start");
+            Example obj1 = new Example();
+            Example obj2 = new Example();
+            Console.WriteLine("Main Method Execution End");
+            Console.ReadLine();
         }
+        //static void Main(string[] args)
+        //{
+        //    //The Abstract Factory Design Pattern provides a way to encapsulate a group of individual factories that have a common theme without specifying their concrete classes
+        //    //Online Source FrondEnd Course
+        //    OnlineSourceFrondEndCourseFactory onlineSourceFrondEndCourseFactory = new OnlineSourceFrondEndCourseFactory();
+        //    Console.WriteLine("Source" + onlineSourceFrondEndCourseFactory.GetIsource().getSourceName());
+        //    Console.WriteLine("Course Duration" + onlineSourceFrondEndCourseFactory.GetCourse().GetCourseDuration());
+        //    Console.WriteLine("Course Name" + onlineSourceFrondEndCourseFactory.GetCourse().getCourseName());
+        //    Console.WriteLine("Course Fee" + onlineSourceFrondEndCourseFactory.GetCourse().getCourseFee());
+
+        //    //Offline Source BackEnd Course
+        //    OfflineSourceBackEndCourse offlineSourceBackEndCourse = new OfflineSourceBackEndCourse();
+        //    Console.WriteLine("Source" + offlineSourceBackEndCourse.GetIsource().getSourceName());
+        //    Console.WriteLine("Course Duration" + offlineSourceBackEndCourse.GetCourse().GetCourseDuration());
+        //    Console.WriteLine("Course Name" + offlineSourceBackEndCourse.GetCourse().getCourseName());
+        //    Console.WriteLine("Course Fee" + offlineSourceBackEndCourse.GetCourse().getCourseFee());
+
+        //    Console.WriteLine("Hello World!");          
+        //}
     }
 }
